b7c579c45df4600c9fc3ca2bc8d5c89430f18d76
jobiw <jobiwankenobi37@gmail.com>
2023-03-11 15:56:35 +0100

    Zadanie 3 lab 1

--- zd.3.py
+++ zd.3.py
@@ -1,5 +1,5 @@
 liczba = int(input("Podaj liczbę, którą chcesz sprawdzić czy znajduję się ona w tablicy: "))
-tablica = [-3,4,5,23,-55,435,-666,2137,420]
+tablica  = [-3,4,5,23,-55,435,-666,2137,420]
 if liczba in tablica:
     print("Podana liczba znajduje się w tablicy")
 else:
