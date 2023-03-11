6709fbd044780001eac7c15006580790979310ba
jobiw <jobiwankenobi37@gmail.com>
2023-03-11 16:53:46 +0100

    Zadanie 4 lab 1

--- zd.4.py
+++ zd.4.py
@@ -1,6 +1,6 @@
-tablica = [-1,3,-4,-82,32,54,-91,66]
-najm = tablica[0]
-for i in tablica:
+t = [-1,3,-4,-82,32,54,-91,66]
+najm = t[0]
+for i in t:
     if najm >= i:
         najm = i
 print("najmniejsza liczba w liście to :",najm)
