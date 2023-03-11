e5d45729034fdcd0e1371ead36de977bea63d639
jobiw <jobiwankenobi37@gmail.com>
2023-03-11 15:54:58 +0100

    lab 1

---
+++ zd.4.py
@@ -0,0 +1,13 @@
+'''tablica=[1,2,34,5,6,7]
+minimum=tablica[0]
+for i in tablica:
+    if i<=minimum:
+        minimum=tablica[i]
+print("Najmniejsza liczba to: ",minimum)'''
+tab=[-3,-4,-9,0,1,3,15,90,9,17,-1]
+naj=tab[0]
+for x in tab:
+    if x<=naj:
+        naj=tab[x]
+
+print(f"najmniejsza liczba w liście to : {naj}")
