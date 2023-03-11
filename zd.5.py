0e544ae3363500c967b9b5ce3de5a78e83db47eb
jobiw <jobiwankenobi37@gmail.com>
2023-03-11 16:32:32 +0100

    Zadanie 5 lab 1

--- zd.5.py
+++ zd.5.py
@@ -1,7 +1,7 @@
 tablica = [[5,2,3],[22,5,6]]
-print(tablica)
+print("Tablica dla porównania",tablica)
 for i in tablica:
     najm = min(i)
     z = i.index(najm)
     i[0], i[z] = i[z], i[0]
-print(tablica)
+print("Tablica po zmianach",tablica)
