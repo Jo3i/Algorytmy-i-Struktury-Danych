996ecd040cd04aa78095cc761bebef2ab490f2f9
Jo3i <115739365+Jo3i@users.noreply.github.com>
2023-04-18 14:43:07 +0200

    ASiD

---
+++ LAB6/zd1.py
@@ -0,0 +1,7 @@
+i=0
+c=0
+for i in range(10):
+    a = int(input("Podaj liczbę: "))
+    c=c+a
+z=c/10
+print(z)
---
+++ LAB6/zd2.py
@@ -0,0 +1,6 @@
+a = int(input("Podaj pierwszą liczbę graniczną przedziału: "))
+b = int(input("Podaj drugą liczbę graniczną przedziału: "))
+while a<=b:
+    if a%2==0:
+        print(a)
+    a=a+1
